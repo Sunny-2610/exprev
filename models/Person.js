44f74e3d359a278453c9ac62cfcd02e86cdb851d
Sunny-2610 <sunnysinha9884@gmail.com>
2025-07-28 15:44:11 +0530

    Completed Database section

--- models/Person.js
+++ models/Person.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const personSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    age:{type:Number, required:true},
-    email:{type:String, required:true, unique:true},
-})
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true, unique: true },
+  userOrder: { type: Object, default: { test: 'test value' } }
+}, { timestamps: true }); // ← moved inside Schema() constructor
 
-  export const Person = mongoose.model("Person",personSchema)
+export const Person = mongoose.model("Person", personSchema);
