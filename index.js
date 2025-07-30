62ad277cfb9d5fdbcc2c77d405ab9a014418cbfe
Sunny-2610 <sunnysinha9884@gmail.com>
2025-07-30 16:07:32 +0530

    learned about cookies

--- index.js
+++ index.js
@@ -1,65 +1,36 @@
 import express from 'express'
-import { connectDB } from './config/db.js';
-import { Person } from './models/Person.js';
+import cookieParser from 'cookie-parser';
+
+
 
 
 const app = express();
 const PORT = 3000; // or any other port
 
-await connectDB()
+app.use(cookieParser())
 
 
 app.use(express.json())
 app.get('/',(req,res)=>{
 
+  res.cookie('name','express-app',{maxAge: 360000})
+
   res.send('Hello,Sunny')
 })
-//Saving data in mongodb
-app.post('/person',async(req,res)=>{
-     
-  try {
-      
-    const {email,name,age} = req.body
+
+ app.get('/fetch',(req,res)=>{
     
-     const newPerson = new Person({
-        name,
-        age,
-        email
-     })
-     await newPerson.save()
-     console.log(newPerson)
-     res.send('Person Added')
-  } catch (error) {
-    res.send(error.message)
-  }
-})
+     console.log(req.cookies)
+     res.send('APi Called')
+
+ })
+
+    app.get('/remove-cookie',(req,res)=>{
+     res.clearCookie('name')
+     res.send('Cookie cleared')
+    })
 
 
-//updating aad in mongodb
-
-
-//updating aad in mongodb
-app.put('/person',async(req,res)=>{
-     
-    
-    const {id} = req.body
-    const personData =  await Person.findByIdAndUpdate(id,{age:'44'})
-    //modifying the data
-   
-    console.log(personData)
-     
-     res.send('Person Updated')
-})
-
-
-
-//deleted Data from MongoDb
-
-app.delete('/person/:id',async (req,res)=>{
-    const {id} =req.params
-    await Person.findByIdAndDelete(id)
-    res.send('User Deleted')
-})
 
 
 app.listen(PORT, () => {
