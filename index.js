5b45d7869e755e53b09a07d02c108da4a3e0347a
Sunny-2610 <sunnysinha9884@gmail.com>
2025-07-31 11:47:48 +0530

    session done

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 import express from 'express'
 import cookieParser from 'cookie-parser';
 
-
+import session from 'express-session';
 
 
 const app = express();
 const PORT = 3000; // or any other port
 
 app.use(cookieParser())
-
+app.use(session({
+  secret:'smaple-secret',
+  resave:false,
+  saveUninitialized:false
+}))
 
 app.use(express.json())
 app.get('/',(req,res)=>{
 
-  res.cookie('name','express-app',{maxAge: 360000})
-
   res.send('Hello,Sunny')
 })
 
- app.get('/fetch',(req,res)=>{
-    
-     console.log(req.cookies)
-     res.send('APi Called')
-
- })
-
-    app.get('/remove-cookie',(req,res)=>{
-     res.clearCookie('name')
-     res.send('Cookie cleared')
+    app.get('/visit',(req,res)=>{
+        if(req.session.page_views) {
+          req.session.page_views++;
+          res.send(`You Visited this page ${req.session.page_views} times`)
+        } else {
+          req.session.page_views = 1
+          res.send("welcome to the page for the first time !")
+        }
     })
 
+
+   app.get('/remove-visit',(req,res)=>{
+        req.session.destroy()
+        res.send('Session Removes')
+   })
 
 
 
